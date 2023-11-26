e5e45c62637634f0169bf561faa554c9b03eff98
kmseongmin <newsungk7@naver.com>
2023-11-26 21:16:30 +0900

    dd

--- Packcap.c
+++ Packcap.c
@@ -10,7 +10,33 @@
 #include <sys/socket.h>       //소켓의 주소 설정 (sockaddr 구조체)
 #include <arpa/inet.h>        //network 정보 변환
 #include <pthread.h>          //thread
+#include <unistd.h>
 
-int main(){
-    
+#define PACKET_SIZE 65536
+
+int main() {
+    int raw_socket;
+    unsigned char buffer[PACKET_SIZE];
+
+    raw_socket = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+
+    if (raw_socket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        ssize_t packet_size = recvfrom(raw_socket, buffer, PACKET_SIZE, 0, NULL, NULL);
+
+        if (packet_size == -1) {
+            perror("Packet reception failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Received a packet of size %zd bytes\n", packet_size);
+    }
+
+    close(raw_socket);
+
+    return 0;
 }
