0f22dab86d80687f2bbc544913a200c0f27e0a4b
junsu1211 <105295416+junsu1211@users.noreply.github.com>
2023-11-26 11:41:16 +0000

    new commit

---
+++ Packcap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>            //basic
+#include <time.h>             //저장한 file 이름을 현재 날짜로 하기 위해
+#include <stdlib.h>           //malloc 동적할당
+#include <string.h>           //memset 초기화
+#include <netinet/if_ether.h> //etherrnet 구조체
+#include <netinet/ip.h>       //ip header 구조체
+#include <netinet/tcp.h>      //tcp header 구조체
+#include <netinet/udp.h>      //udp header 구조체
+#include <netinet/ip_icmp.h>  //icmp header 구조체
+#include <sys/socket.h>       //소켓의 주소 설정 (sockaddr 구조체)
+#include <arpa/inet.h>        //network 정보 변환
+#include <pthread.h>          //thread
+
+#include 
